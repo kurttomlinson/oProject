0439b0140ce4eabc94d1d2381a3e59ee5d2d0a55
Kurt <kurttomlinson@gmail.com>
2012-07-28 22:43:05 -0500

    added documentation

---
+++ Documentation/scripts/xmlstudio.js
@@ -0,0 +1,320 @@
+﻿function EnsureInFrame(itemType, selectedItemType) {
+    hljs.tabReplace = ' ';
+    hljs.initHighlightingOnLoad();
+
+    if (window.name != 'contentFrame') {
+        /* if were not open in a frame - load the frame */
+        document.location = 'index.html?url=' + document.location.pathname;
+    }
+    else {
+        $(function () {
+            //set initial state of the collapsable sections
+            setState('.expand');
+
+            $('.expand').click(function () {
+                expandContract(this);
+            });
+
+            if ($.urlParam('sync') == '') {
+                if (window.parent.frames["_Menu"] != null && itemType != null && selectedItemType != null) {
+                    window.parent.frames["_Menu"].selectItem(itemType, selectedItemType);
+                }
+            }
+        });
+    }
+}
+function setState(selector) {
+
+    $(selector).each(function () {
+        var id = $(this).attr('id');
+        if ($.cookie(id) == 0)
+            expandContract(this);
+    });
+}
+
+function expandContract(el) {
+
+    var toggleMinus = 'images/minus.gif'
+    var togglePlus = 'images/plus.gif'
+
+    if ($(el).attr('src') == toggleMinus) {
+        $(el).parents('tr').siblings().hide();
+        $(el).attr('src', togglePlus);
+        $.cookie($(el).attr('id'), '0');
+    } else {
+        $(el).parents('tr').siblings().show();
+        $(el).attr('src', toggleMinus);
+        $.cookie($(el).attr('id'), '1');
+    }
+}
+
+var persisteduls = new Object()
+var ddtreemenu = new Object()
+
+ddtreemenu.closefolder = "closed.gif" //set image path to "closed" folder image
+ddtreemenu.openfolder = "open.gif" //set image path to "open" folder image
+
+//////////No need to edit beyond here///////////////////////////
+
+ddtreemenu.createTree = function (treeid, enablepersist, persistdays) {
+    var ultags = document.getElementById(treeid).getElementsByTagName("ul")
+    if (typeof persisteduls[treeid] == "undefined")
+        persisteduls[treeid] = (enablepersist == true && ddtreemenu.getCookie(treeid) != "") ? ddtreemenu.getCookie(treeid).split(",") : ""
+    for (var i = 0; i < ultags.length; i++)
+        ddtreemenu.buildSubTree(treeid, ultags[i], i)
+    if (enablepersist == true) { //if enable persist feature
+        var durationdays = (typeof persistdays == "undefined") ? 1 : parseInt(persistdays)
+        ddtreemenu.dotask(window, function () { ddtreemenu.rememberstate(treeid, durationdays) }, "unload") //save opened UL indexes on body unload
+    }
+}
+
+ddtreemenu.buildSubTree = function (treeid, ulelement, index) {
+    ulelement.parentNode.className = "submenu"
+    if (typeof persisteduls[treeid] == "object") { //if cookie exists (persisteduls[treeid] is an array versus "" string)
+        if (ddtreemenu.searcharray(persisteduls[treeid], index)) {
+            ulelement.setAttribute("rel", "open")
+            ulelement.style.display = "block"
+            ulelement.parentNode.style.backgroundImage = "url(" + ddtreemenu.openfolder + ")"
+        }
+        else
+            ulelement.setAttribute("rel", "closed")
+    } //end cookie persist code
+    else if (ulelement.getAttribute("rel") == null || ulelement.getAttribute("rel") == false) //if no cookie and UL has NO rel attribute explicted added by user
+        ulelement.setAttribute("rel", "closed")
+    else if (ulelement.getAttribute("rel") == "open") //else if no cookie and this UL has an explicit rel value of "open"
+        ddtreemenu.expandSubTree(treeid, ulelement) //expand this UL plus all parent ULs (so the most inner UL is revealed!)
+    ulelement.parentNode.onclick = function (e) {
+        var submenu = this.getElementsByTagName("ul")[0]
+        if (submenu.getAttribute("rel") == "closed") {
+            submenu.style.display = "block"
+            submenu.setAttribute("rel", "open")
+            ulelement.parentNode.style.backgroundImage = "url(" + ddtreemenu.openfolder + ")"
+        }
+        else if (submenu.getAttribute("rel") == "open") {
+            submenu.style.display = "none"
+            submenu.setAttribute("rel", "closed")
+            ulelement.parentNode.style.backgroundImage = "url(" + ddtreemenu.closefolder + ")"
+        }
+        ddtreemenu.preventpropagate(e)
+    }
+    ulelement.onclick = function (e) {
+        ddtreemenu.preventpropagate(e)
+    }
+}
+
+ddtreemenu.expandSubTree = function (treeid, ulelement) { //expand a UL element and any of its parent ULs
+    var rootnode = document.getElementById(treeid)
+    var currentnode = ulelement
+    currentnode.style.display = "block"
+    currentnode.parentNode.style.backgroundImage = "url(" + ddtreemenu.openfolder + ")"
+    while (currentnode != rootnode) {
+        if (currentnode.tagName == "UL") { //if parent node is a UL, expand it too
+            currentnode.style.display = "block"
+            currentnode.setAttribute("rel", "open") //indicate it's open
+            currentnode.parentNode.style.backgroundImage = "url(" + ddtreemenu.openfolder + ")"
+        }
+        currentnode = currentnode.parentNode
+    }
+}
+
+ddtreemenu.flatten = function (treeid, action) { //expand or contract all UL elements
+    var ultags = document.getElementById(treeid).getElementsByTagName("ul")
+    for (var i = 0; i < ultags.length; i++) {
+        ultags[i].style.display = (action == "expand") ? "block" : "none"
+        var relvalue = (action == "expand") ? "open" : "closed"
+        ultags[i].setAttribute("rel", relvalue)
+        ultags[i].parentNode.style.backgroundImage = (action == "expand") ? "url(" + ddtreemenu.openfolder + ")" : "url(" + ddtreemenu.closefolder + ")"
+    }
+}
+
+ddtreemenu.rememberstate = function (treeid, durationdays) { //store index of opened ULs relative to other ULs in Tree into cookie
+    var ultags = document.getElementById(treeid).getElementsByTagName("ul")
+    var openuls = new Array()
+    for (var i = 0; i < ultags.length; i++) {
+        if (ultags[i].getAttribute("rel") == "open")
+            openuls[openuls.length] = i //save the index of the opened UL (relative to the entire list of ULs) as an array element
+    }
+    if (openuls.length == 0) //if there are no opened ULs to save/persist
+        openuls[0] = "none open" //set array value to string to simply indicate all ULs should persist with state being closed
+    ddtreemenu.setCookie(treeid, openuls.join(","), durationdays) //populate cookie with value treeid=1,2,3 etc (where 1,2... are the indexes of the opened ULs)
+}
+
+////A few utility functions below//////////////////////
+
+ddtreemenu.getCookie = function (Name) { //get cookie value
+    var re = new RegExp(Name + "=[^;]+", "i"); //construct RE to search for target name/value pair
+    if (document.cookie.match(re)) //if cookie found
+        return document.cookie.match(re)[0].split("=")[1] //return its value
+    return ""
+}
+
+ddtreemenu.setCookie = function (name, value, days) { //set cookei value
+    var expireDate = new Date()
+    //set "expstring" to either future or past date, to set or delete cookie, respectively
+    var expstring = expireDate.setDate(expireDate.getDate() + parseInt(days))
+    document.cookie = name + "=" + value + "; expires=" + expireDate.toGMTString() + "; path=/";
+}
+
+ddtreemenu.searcharray = function (thearray, value) { //searches an array for the entered value. If found, delete value from array
+    var isfound = false
+    for (var i = 0; i < thearray.length; i++) {
+        if (thearray[i] == value) {
+            isfound = true
+            thearray.shift() //delete this element from array for efficiency sake
+            break
+        }
+    }
+    return isfound
+}
+
+ddtreemenu.preventpropagate = function (e) { //prevent action from bubbling upwards
+    if (typeof e != "undefined")
+        e.stopPropagation()
+    else
+        event.cancelBubble = true
+}
+
+ddtreemenu.dotask = function (target, functionref, tasktype) { //assign a function to execute to an event handler (ie: onunload)
+    var tasktype = (window.addEventListener) ? tasktype : "on" + tasktype
+    if (target.addEventListener)
+        target.addEventListener(tasktype, functionref, false)
+    else if (target.attachEvent)
+        target.attachEvent(tasktype, functionref)
+}
+
+
+function setTabs() {
+    //Default Action
+    $(".tab_content").hide(); //Hide all content
+    $("ul.tabs li:first").addClass("active").show(); //Activate first tab
+    $(".tab_content:first").show(); //Show first tab content
+
+    //On Click Event
+    $("ul.tabs li").click(function () {
+        $("ul.tabs li").removeClass("active"); //Remove any "active" class
+        $(this).addClass("active"); //Add "active" class to selected tab
+        $(".tab_content").hide(); //Hide all tab content
+        var activeTab = $(this).find("a").attr("href"); //Find the rel attribute value to identify the active tab + content
+        $(activeTab).fadeIn(); //Fade in the active content
+        return false;
+    });
+}
+
+$.urlParam = function (name) {
+    var results = new RegExp('[\\?&]' + name + '=([^&#]*)').exec(window.location.href);
+    if (!results) { return 0; }
+    return results[1] || 0;
+}
+
+function trim(str, chars) {
+    return ltrim(rtrim(str, chars), chars);
+}
+
+function ltrim(str, chars) {
+    chars = chars || "\\s";
+    return str.replace(new RegExp("^[" + chars + "]+", "g"), "");
+}
+
+function rtrim(str, chars) {
+    chars = chars || "\\s";
+    return str.replace(new RegExp("[" + chars + "]+$", "g"), "");
+}
+
+function unique(a) {
+    var r = new Array();
+    o: for (var i = 0, n = a.length; i < n; i++) {
+        for (var x = 0, y = r.length; x < y; x++) {
+            if (r[x] == a[i]) continue o;
+        }
+        r[r.length] = a[i];
+    }
+    return r;
+}
+
+/* Loads the root elements in the tree */
+function loadTree() {
+    if ($("#ulMenu").attr('loaded') != 'true') {
+        $.ajax({
+            url: 'menudata_root.html',
+            success: function (data) {
+                $("#ulMenu").html(data);
+                $("#ulMenu").attr('loaded', 'true');
+            }
+        });
+    }
+}
+
+/* Selects a give item, ensuring the container node is loaded first */
+function toggleNode(itemType) {
+    var el = $("#" + itemType + " ul");
+    var img = $("#" + itemType + " img");
+
+    if (el.attr('loaded') != 'true') {
+        $.ajax({
+            url: 'menudata_node_' + itemType + '.html',
+            success: function (data) {
+                el.html(data);
+                el.attr('loaded', 'true');
+                el.attr('expanded', 'true');
+                $(img).attr('src', 'images/minus.gif');
+            }
+        });
+    }
+    else {
+        if (el.attr('expanded') == 'true') {
+            el.attr('expanded', 'false');
+            $(img).attr('src', 'images/plus.gif');
+            el.hide();
+        }
+        else {
+            el.attr('expanded', 'true');
+            $(img).attr('src', 'images/minus.gif');
+            el.show();
+        }
+    }
+}
+
+
+/* Selects a give item, ensuring the container node is loaded first */
+function selectItem(itemType, selectedItemType) {
+    var el = $("#" + itemType + " ul");
+    var img = $("#" + itemType + " img");
+
+    if (el.attr('loaded') != 'true') {
+        $.ajax({
+            url: 'menudata_node_' + itemType + '.html',
+            success: function (data) {
+                el.html(data);
+                el.attr('loaded', 'true');
+                el.attr('expanded', 'true');
+                setHighlightedItem(selectedItemType);
+                $(img).attr('src', 'images/minus.gif');
+                try {
+                    $('html,body').animate({ scrollTop: $("#" + selectedItemType).offset().top - 30 }, 'fast');
+                }
+                catch (ex)
+                { }
+            }
+        });
+    }
+    else {
+        el.attr('expanded', 'true');
+        el.show();
+        setHighlightedItem(selectedItemType);
+        $(img).attr('src', 'images/minus.gif');
+        try {
+            $('html,body').animate({ scrollTop: $("#" + selectedItemType).offset().top - 30 }, 'fast');
+        }
+        catch (ex)
+                { }
+
+    }
+}
+
+function setHighlightedItem(selectedItemType) {
+    $("li").removeClass("selected");
+    $("#" + selectedItemType).addClass("selected");
+}
+
+
+
---
+++ Documentation/searchindex.js
@@ -0,0 +1,152 @@
+
+function CreateFileRefs()
+{
+var fr=new Array();
+            
+fr[1]=['oProject.xsd', 'oproject_xsd.html'];
+fr[2]=['typeDownload', 'typedownload.html'];
+fr[3]=['description', 'description.html'];
+fr[4]=['order', 'order.html'];
+fr[5]=['url', 'url.html'];
+fr[6]=['typeDownloads', 'typedownloads.html'];
+fr[7]=['file', 'file.html'];
+fr[8]=['typeExternalReference', 'typeexternalreference.html'];
+fr[9]=['external_id', 'external_id.html'];
+fr[10]=['external_source', 'external_source.html'];
+fr[11]=['typeImage', 'typeimage.html'];
+fr[12]=['description', 'description1.html'];
+fr[13]=['order', 'order1.html'];
+fr[14]=['url', 'url1.html'];
+fr[15]=['typeImages', 'typeimages.html'];
+fr[16]=['image', 'image.html'];
+fr[17]=['typeImageSingle', 'typeimagesingle.html'];
+fr[18]=['description', 'description2.html'];
+fr[19]=['url', 'url2.html'];
+fr[20]=['typeLicense', 'typelicense.html'];
+fr[21]=['name', 'name.html'];
+fr[22]=['url', 'url3.html'];
+fr[23]=['typeManifest', 'typemanifest.html'];
+fr[24]=['url', 'url4.html'];
+fr[25]=['typePart', 'typepart.html'];
+fr[26]=['description', 'description3.html'];
+fr[27]=['external_reference', 'external_reference.html'];
+fr[28]=['image', 'image1.html'];
+fr[29]=['quantity', 'quantity.html'];
+fr[30]=['SKU', 'sku.html'];
+fr[31]=['order', 'order2.html'];
+fr[32]=['url', 'url5.html'];
+fr[33]=['typeParts', 'typeparts.html'];
+fr[34]=['part', 'part.html'];
+fr[35]=['typeProject', 'typeproject.html'];
+fr[36]=['downloads', 'downloads.html'];
+fr[37]=['manifest', 'manifest.html'];
+fr[38]=['parts', 'parts.html'];
+fr[39]=['project_description', 'project_description.html'];
+fr[40]=['steps', 'steps.html'];
+fr[41]=['typeProjectDescription', 'typeprojectdescription.html'];
+fr[42]=['author', 'author.html'];
+fr[43]=['description', 'description4.html'];
+fr[44]=['images', 'images.html'];
+fr[45]=['license', 'license.html'];
+fr[46]=['name', 'name1.html'];
+fr[47]=['video', 'video.html'];
+fr[48]=['typeStep', 'typestep.html'];
+fr[49]=['description', 'description5.html'];
+fr[50]=['images', 'images1.html'];
+fr[51]=['title', 'title.html'];
+fr[52]=['order', 'order3.html'];
+fr[53]=['typeSteps', 'typesteps.html'];
+fr[54]=['step', 'step.html'];
+fr[55]=['typeVideo', 'typevideo.html'];
+fr[56]=['description', 'description6.html'];
+fr[57]=['url', 'url6.html'];
+fr[58]=['project', 'project.html'];
+fr[59]=['typeExternalSource', 'typeexternalsource.html'];    
+ return fr;          
+}
+
+function CreateWordIndex()
+{
+var w=[
+            
+['--created',[1]],
+['with',[1]],
+['liquid',[1]],
+['studio',[1]],
+['developer',[1]],
+['edition',[1]],
+['trial',[1]],
+['http',[1]],
+['liquid-technologies',[1]],
+['schema',[1]],
+['elementformdefault',[1]],
+['qualified',[1]],
+['version',[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59]],
+['xmlns',[1]],
+['xmlschema',[1]],
+['element',[1,2,3,6,7,8,9,10,11,12,15,16,17,18,20,21,25,26,27,28,29,30,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,53,54,55,56,58]],
+['name',[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59]],
+['project',[1,35,39,58]],
+['type',[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58]],
+['typeproject',[1,35,58]],
+['complextype',[1,2,6,8,11,15,17,20,23,25,33,35,41,48,53,55]],
+['manifest',[1,35,37,58]],
+['typemanifest',[1,23,35,37]],
+['minoccurs',[1,6,7,11,12,15,16,17,18,25,27,28,35,36,37,38,39,40,41,45,48,50,53,54,55,56]],
+['parts',[1,35,38,58]],
+['typeparts',[1,33,35,38]],
+['steps',[1,35,40,58]],
+['typesteps',[1,35,40,53]],
+['downloads',[1,35,36,58]],
+['typedownloads',[1,6,35,36]],
+['description',[1,2,3,7,11,12,16,17,18,25,26,28,34,35,39,41,43,47,48,49,54,55,56,58]],
+['typeprojectdescription',[1,35,39,41]],
+['sequence',[1,6,15,33,53]],
+['part',[1,33,34,38]],
+['typepart',[1,25,33,34]],
+['maxoccurs',[1,6,7,15,16,33,34,53,54]],
+['unbounded',[1,6,7,15,16,33,34,36,38,40,44,50,53,54]],
+['normalizedstring',[1,2,3,8,9,20,21,25,30,41,42,46,48,51]],
+['string',[1,25,26,41,43,48,49]],
+['quantity',[1,25,29,34]],
+['positiveinteger',[1,2,4,11,13,25,29,31,48,52]],
+['image',[1,15,16,25,28,34,44,50]],
+['typeimagesingle',[1,17,25,28]],
+['external',[1,8,9,10,25,27,34]],
+['reference',[1,25,27,34]],
+['typeexternalreference',[1,8,25,27]],
+['attribute',[1,2,4,5,11,13,14,17,19,20,22,23,24,25,31,32,48,52,55,57]],
+['anyuri',[1,2,5,11,14,17,19,20,22,23,24,25,32,55,57]],
+['required',[1,2,4,5,7,11,13,14,16,17,19,20,22,25,28,31,32,34,45,48,52,54]],
+['order',[1,2,4,7,11,13,16,25,31,34,48,52,54]],
+['typeimages',[1,15,41,44,48,50]],
+['typeimage',[1,11,15,16]],
+['token',[1,11,12,17,18,55,56,59]],
+['source',[1,8,10,27]],
+['typeexternalsource',[1,8,10,59]],
+['simpletype',[1,59]],
+['restriction',[1,59]],
+['base',[1,59]],
+['enumeration',[1,59]],
+['value',[1,59]],
+['octopart',[1,59]],
+['digikey',[1,59]],
+['step',[1,40,53,54]],
+['typestep',[1,48,53,54]],
+['title',[1,48,51,54]],
+['images',[1,39,41,44,48,50,54]],
+['file',[1,6,7,36]],
+['typedownload',[1,2,6,7]],
+['author',[1,39,41,42]],
+['license',[1,39,41,45]],
+['typelicense',[1,20,41,45]],
+['video',[1,39,41,47]],
+['typevideo',[1,41,47,55]],
+['false',[2,3,6,7,8,9,10,11,12,15,16,17,18,20,21,23,25,26,27,28,29,30,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,53,54,55,56,58]],
+['encoding',[2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59]],
+['utf-16',[2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59]],
+['optional',[23,24,37,47,55,57]]
+ ];
+ return w;
+}
+        
